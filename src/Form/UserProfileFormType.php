77c92c41928b99ce262cc5201ee2515a41827de9
Naklish <iglesias.charles@outlook.com>
2021-03-16 17:55:18 +0100

    Gestion profil.

---
+++ src/Form/UserProfileFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo :'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom :'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom :'
+            ])
+            ->add('phone', NumberType::class, [
+                'label' => 'Téléphone :'
+            ])
+            ->add('mail', EmailType::class, [
+                'label' => 'E-mail'
+            ])
+            ->add('campus')
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe :'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
