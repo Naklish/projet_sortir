d25a3419cdd0426139d93f23c456c99d65b1603b
ClementLeMalefan <clement.le.malefan@gmail.com>
2021-03-18 10:58:05 +0100

    màj formulaire outing

--- src/Form/OutingFormType.php
+++ src/Form/OutingFormType.php
@@ -3,17 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Campus;
-use App\Entity\City;
 use App\Entity\Location;
 use App\Entity\Outing;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\View\ChoiceView;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -53,6 +49,7 @@ class OutingFormType extends AbstractType
                 'label' => 'Lieu :'
             ])
 
+            // il faut rajouter l'option : 'mapped' => false,
         ;
     }
 
